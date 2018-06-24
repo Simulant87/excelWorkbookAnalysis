1f919c763b7e85de65f22a45ed17e077e892063b
Simulant <simulant88@gmail.com>
2018-06-24 17:07:44 +0200

    process sheets in parallel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,90 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.FileInputStream
 
-fun main(args : Array<String>) {
-    var max = 0
-    var maxSheet = 0
-    var maxRow = 0
-    var maxCell = 0
+/**
+ * reads a path to a file from the first command line argument, tris to the file as excel workbook
+ * and reports the longest String in a cell, highest row and column count per sheet.
+ */
+fun main(args: Array<String>) {
 
-    var path = "src\\main\\resources\\Mappe1.xlsx"
-    if (args.isNotEmpty()) {
-        path = args[0]
-    }
-    val inputStream = FileInputStream(path)
-    val workbook = XSSFWorkbook(inputStream)
-    for(sheetIndex in 0 until workbook.numberOfSheets) {
+    val workbook = createWorkbook(args)
+
+    val numberOfSheets = workbook.numberOfSheets
+    val sheetResults = kotlin.arrayOfNulls<SheetResult>(numberOfSheets)
+    val threads = ArrayList<Thread>()
+
+    for(sheetIndex in 0 until numberOfSheets) {
         val sheet = workbook.getSheetAt(sheetIndex)
         if (sheet != null) {
-            for (rowIndex in 0 until sheet.lastRowNum + 1) {
-                val row = sheet.getRow(rowIndex)
-                if (row != null) {
-                    for (cellIndex in 0 until row.lastCellNum + 1) {
-                        val cell = row.getCell(cellIndex)
-                        if(cell != null) {
-                            try {
-                                val value = cell.stringCellValue
-                                if (value != null) {
-                                    val length = value.length
-                                    println("sheet: $sheetIndex, row: $rowIndex, cell: $cellIndex, length: $length")
-                                    if (length > max) {
-                                        max = length
-                                        maxSheet = sheetIndex
-                                        maxRow = rowIndex
-                                        maxCell = cellIndex
+            //starting a thread for each sheet to analyze the workbook in parallel
+            val thread = Thread { analyzeSheet(sheet, sheetIndex, sheetResults) }
+            thread.start()
+            threads.add(thread)
+        }
+    }
 
-                                    }
-                                }
-                            } catch (e: IllegalStateException) {
-                                println("sheet: $sheetIndex, row: $rowIndex, cell: $cellIndex, ERROR: e.message")
+    //joining all threads started for the analysis to the report is not empty
+    for( thread in threads) {
+        thread.join()
+    }
+
+    for (sheetResult in sheetResults) {
+        println(sheetResult)
+    }
+}
+
+private fun createWorkbook(args: Array<String>): Workbook {
+    val path = if (args.isNotEmpty()) {
+        //reading path to workbook to be analyzed from first command line argument
+        args[0]
+    } else {
+        //default fall back path to the workbook in the resources directory
+        """src\main\resources\Mappe1.xlsx"""
+    }
+    println("path to analyze: $path")
+    val inputStream = FileInputStream(path)
+    val workbook = if (path.endsWith(".xls")) {
+        HSSFWorkbook(inputStream)
+    } else {
+        XSSFWorkbook(inputStream)
+    }
+    return workbook
+}
+
+private fun analyzeSheet(sheet: Sheet, sheetIndex: Int, sheetResults: Array<SheetResult?>) {
+    var max = 0
+    var maxRow = 0
+    var maxCell = 0
+    var maxRowCount = 0
+    for (rowIndex in 0 until sheet.lastRowNum + 1) {
+        val row = sheet.getRow(rowIndex)
+        if (row != null) {
+            if (maxRowCount < row.lastCellNum) {
+                maxRowCount = row.lastCellNum.toInt()
+            }
+            for (cellIndex in 0 until row.lastCellNum) {
+                val cell = row.getCell(cellIndex)
+                if (cell != null) {
+                    try {
+                        val value = cell.stringCellValue
+                        if (value != null) {
+                            val length = value.length
+                            println("sheet: $sheetIndex, row: $rowIndex, cell: $cellIndex, length: $length")
+                            if (length > max) {
+                                max = length
+                                maxRow = rowIndex
+                                maxCell = cellIndex
                             }
                         }
+                    } catch (e: IllegalStateException) {
+                        println("sheet: $sheetIndex, row: $rowIndex, cell: $cellIndex, ERROR: e.message")
                     }
                 }
             }
         }
     }
-    println("longest cell is $max characters long at sheet: $maxSheet, row: $maxRow, cell: $maxCell")
+    sheetResults[sheetIndex] = SheetResult(sheetIndex, max, maxRow, maxCell, sheet.lastRowNum, maxRowCount)
 }
